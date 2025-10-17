29adb42aab9cdcb3b65472a1240ee77c93be9821
zDelevigne <blackknightsword01@gmail.com>
2025-10-17 20:26:57 -0300

    TP 1

---
+++ TP-Alumnos/CarrascosaFrancisco_61469/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() 
+{
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <small>(c) 2025 Todos los derechos reservados.</small>
+      </div>
+    </footer>
+  );
+}
+
