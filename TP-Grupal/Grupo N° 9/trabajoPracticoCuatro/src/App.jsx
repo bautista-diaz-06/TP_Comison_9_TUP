fed008de170dc536c4fbb80e129026abc979c00e
Elias <eliasasfoura7@gmail.com>
2025-10-26 23:14:13 -0300

    Tabla Alumnos terminada

--- TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/App.jsx
+++ TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/App.jsx
@@ -2,14 +2,19 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Inicio from "./Containers/Inicio"
 import RegistrarAlumno from "./Containers/RegistrarAlumno"
 import RegistrarLibro from "./Containers/RegistrarLibro"
+import RegistrarPrestamo from "./Containers/RegistrarPrestamo"
+import Alumnos from "./Containers/Alumnos"
+
 function App() {
 
   return (
     <Router>
       <Routes>
         <Route path="/inicio" element={<Inicio/>}/>
+        <Route path="/alumnos" element={<Alumnos/>}/>
         <Route path="/registrarAlumno" element={<RegistrarAlumno/>} />
          <Route path="/registrarLibro" element={<RegistrarLibro/>}/>
+         <Route path="/registrarPrestamo" element={<RegistrarPrestamo/>}/>
       </Routes>
     </Router>
   )
--- TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/Containers/Alumnos/index.jsx
+++ TP-Grupal/Grupo N° 9/trabajoPracticoCuatro/src/Containers/Alumnos/index.jsx
@@ -1,7 +1,13 @@
+import Header from "../../Componets/Header";
+import TablaAlumnos from "./Components/TablaAlumnos";
 
 const Alumnos = () => {
     return(
         <>
+
+        <Header/>
+
+        <TablaAlumnos/>
 
         </>
     )
