ab22db3c3f0ed307e810120d0e08885499a32e34
Camila Almiron <almironcamila848@gmail.com>
2025-11-01 17:46:32 -0300

    Agrego db.json con datos simulados

--- TP-Grupal/Grupo N° 7 - TP 6/src/App.jsx
+++ TP-Grupal/Grupo N° 7 - TP 6/src/App.jsx
@@ -1,23 +1,25 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FormularioLogin from './pages/FormularioLogin'
-import { Routes, Route } from 'react-router-dom'
-import TablaEventos from './pages/TablaEventos'
+import FormularioLogin from './pages/FormularioLogin';
+import { Routes, Route } from 'react-router-dom';
+import TablaEventos from './pages/TablaEventos';
 import TablaArtistas from './pages/TablaArtistas';
 import DashboardPrincipal from './dashboard/DashboardPrincipal';
 import TablaAsistentes from './pages/TablaAsistentes';
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<FormularioLogin />}></Route>
-      <Route path="/tabla-eventos" element={<TablaEventos/>}></Route>
-      <Route path='/tabla-artistas' element={<TablaArtistas/>}></Route>
-      <Route path='/dashboard' element={<DashboardPrincipal />}></Route>
-      <Route path='/tabla-asistentes' element={<TablaAsistentes/>}></Route>
+      {/* Ruta pública de login */}
+      <Route path="/" element={<FormularioLogin />} />
+
+      {/* Rutas para tablas y dashboard (todavía sin protección) */}
+      <Route path="/tabla-eventos" element={<TablaEventos />} />
+      <Route path="/tabla-artistas" element={<TablaArtistas />} />
+      <Route path="/dashboard" element={<DashboardPrincipal />} />
+      <Route path="/tabla-asistentes" element={<TablaAsistentes />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
